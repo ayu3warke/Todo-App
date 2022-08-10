32be243089cb1dfe022a72488207516cc65e21a0
ayu3warke <ayuwarke3@gmail.com>
2022-08-10 18:26:14 +0530

    first commit

---
+++ config/tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*'
+  ],
+  theme: {
+    extend:{}
+
+  },
+  plugins: []
+
+}
